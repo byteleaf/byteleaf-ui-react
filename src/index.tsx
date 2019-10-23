8c83a0f531ed97d250d6407a55900b3f99d67420
Markus Heer <mail@markus-heer.com>
2019-10-23 11:08:24 +0200

    added export statements

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,8 @@
+export { BFormSectionHeadline } from './components/Form/BFormSectionHeadline/BFormSectionHeadline';
+
+export { BButtonGridItem } from './components/Grid/BButtonGridItem/BButtonGridItem';
+
+export { BBoxLayout } from './components/Layout/BBoxLayout/BBoxLayout';
+export { BPaddedPaper } from './components/Layout/BPaddedPaper/BPaddedPaper';
+
 export { BHeadline } from './components/Text/BHeadline/BHeadline';
